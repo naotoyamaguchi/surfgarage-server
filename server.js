37a6659b89096bb3e1e5709ae5b20dfe1b1a802e
Naoto Yamaguchi <naotoyamaguchi@users.noreply.github.com>
2017-07-24 14:06:17 -1000

    Added working routes for POST and GET

--- server.js
+++ server.js
@@ -2,6 +2,33 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 const PORT = process.env.PORT || 3000;
 
+//setting up connection for knex to database
+//this needs to end up on a config file soon.
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host     : 'localhost',
+    user     : 'naotoy',
+    password : null,
+    database : 'surfgarage',
+    charset  : 'utf8'
+  }
+});
+
+var bookshelf = require('bookshelf')(knex);
+
+var Surfboard = bookshelf.Model.extend({
+  tableName: 'Surfboards',
+  name: 'text',
+  feet: 'integer',
+  inches: 'integer',
+  width: 'float',
+  thickness: 'float',
+  shaper: 'text'
+});
+
+// console.log(Surfboard);
+
 server.connection({
 	host: 'localhost',
 	port: PORT
@@ -28,7 +55,53 @@ server.register(require('inert'), (err) => {
 				gender: "Female"
 			}
 			];
-			reply(fakeApi);
+			Surfboard.collection().fetch().then(function(collection) {
+			  reply(collection);
+			});
+		}
+	});
+
+	server.route({
+		method: 'POST',
+		path: '/api/newBoard',
+		handler: function(request, reply){
+			console.log(request.query);
+			new Surfboard({
+				name: request.query.name,
+				feet: request.query.feet,
+				inches: request.query.inches,
+				width: request.query.width,
+				thickness: request.query.thickness,
+				shaper: request.query.shaper,
+				fins: request.query.fins,
+				createdAt: new Date(),
+				updatedAt: new Date()
+			})
+			.save(null, {method: 'insert'})
+			.then(function(model) {
+				reply(model);
+			});
+		}
+	});
+
+	server.route({
+		method: 'DELETE',
+		path: '/api/deleteBoard/{id}',
+		handler: function(request, reply){
+			Surfboard.forge({id: encodeURIComponent(request.params.id)})
+			.fetch({require: true})
+			.then(function(board){
+				board.destroy()
+				.then(function(){
+					reply({error: false, data: {message: 'Board successfully deleted!'}});
+				})
+				.catch(function(err) {
+					reply({error: true, data: {message: err.message}});
+				});
+			})
+			.catch(function(err) {
+				reply({error: true, data: {message: err.message}});
+			});
 		}
 	});
 });
